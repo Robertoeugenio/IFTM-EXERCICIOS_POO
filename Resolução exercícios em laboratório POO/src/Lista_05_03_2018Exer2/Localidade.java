28401155f16113f7fd22a3e5a0113f72165ca332
Roberto Eugenio dos Santos <30935436+Robertoeugenio@users.noreply.github.com>
2018-03-06 18:47:38 -0300

    Delete Localidade.java

--- Resolução exercícios em laboratório POO/src/Lista_05_03_2018Exer2/Localidade.java
+++
@@ -1,9 +0,0 @@
-package Lista_05_03_2018Exer2;
-
-import java.util.List;
-
-public class Localidade {
-    private String nome;
-    private String facilidadeDeTransporte;
-    private List<Localidade> localdiadeVizinhas;
-}
