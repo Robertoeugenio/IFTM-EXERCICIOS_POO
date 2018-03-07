91aa2cc60af62c5c5e8d3ccb00b3d1e4b60ac5fb
Roberto Eugenio dos Santos <30935436+Robertoeugenio@users.noreply.github.com>
2018-03-06 22:35:33 -0300

    Add files via upload

---
+++ Resolução exercícios em laboratório POO/src/Lista_06_03_2018exer2/Amigo.java
@@ -0,0 +1,5 @@
+package Lista_06_03_2018exer2;
+
+public class Amigo extends Pessoa{
+	private String diaAniversario;
+}
---
+++ Resolução exercícios em laboratório POO/src/Lista_06_03_2018exer2/Pessoa.java
@@ -0,0 +1,8 @@
+package Lista_06_03_2018exer2;
+
+public class Pessoa {
+	private String nome;
+	private String sexo;
+	private int idade;
+
+}
