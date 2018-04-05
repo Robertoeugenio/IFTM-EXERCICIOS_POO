4997e2b2ff532c731bd7f79f35e59cdbcc222310
Roberto Eugenio dos Santos <30935436+Robertoeugenio@users.noreply.github.com>
2018-04-05 15:09:40 -0300

    Add files via upload

---
+++ CalculadoraDePrecos.java
@@ -0,0 +1,12 @@
+
+public class CalculadoraDePrecos {
+	private TabelaDePreco1 tabela;
+	private ServicoDeEntrega entrega;
+
+	public CalculadoraDePrecos(
+
+			TabelaDePreco1 tabela, ServicoDeEntrega entrega) {
+		this.tabela = tabela;
+		this.entrega = entrega;
+	}
+}
